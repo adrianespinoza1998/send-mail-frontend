bc25f650434a1248797e09af1924238302acb199
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2023-01-02 19:35:56 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import SendMailScreen from './components/screens/SendMailScreen'
+
+function App() {
+
+  return (
+    <SendMailScreen />
+  )
+}
+
+export default App
---
+++ src/components/screens/SendMailScreen.tsx
@@ -0,0 +1,105 @@
+import { Box, Button, Container, createTheme, CssBaseline, TextField, ThemeProvider, Typography } from "@mui/material";
+import { useCallback, useState } from "react";
+
+const theme = createTheme();
+
+const SendMailScreen = () => {
+    const [form, setForm] = useState({
+        email: '',
+        asunto: '',
+        cuerpo: ''
+    });
+
+    const [validarMail, setValidarMail] = useState(false);
+
+    const { email, asunto, cuerpo } = form;
+    
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const handleChange = useCallback((e: any) => {
+        if(e.target.name === 'email'){
+            setValidarMail(!emailRegex.test(e.target.value));
+        }
+
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    }, [form, validarMail]);
+
+    const handleSubmit = useCallback(async(e: any) => {
+        e.preventDefault();
+        console.log(form);
+    }, [form]);
+
+  return (
+    <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+                <Typography component="h1" variant="h5">
+                    Enviar correo electronico
+                </Typography>
+                <Box 
+                    component="form"
+                    noValidate sx={{ mt: 1 }}
+                    onSubmit={handleSubmit}
+                >
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        label="Correo electronico"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                        autoComplete="email"
+                        autoFocus
+                        error={validarMail}
+                        helperText={validarMail ? 'Correo electronico no valido' : ''}
+                        type="email"
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        label="Asunto"
+                        name="asunto"
+                        value={asunto}
+                        onChange={handleChange}
+                        type="text"
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="cuerpo"
+                        label="Cuerpo del correo"
+                        type="text"
+                        value={cuerpo}
+                        onChange={handleChange}
+                        minRows={3}
+                        multiline 
+                    />
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                    >
+                      Enviar
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
+    </ThemeProvider>
+  )
+}
+
+export default SendMailScreen;
