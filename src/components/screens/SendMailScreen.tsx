b776d16736def606e9e9d3425d4c1de988aa8d1e
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2023-01-08 20:59:36 -0300

    send mail fetch

--- src/components/screens/SendMailScreen.tsx
+++ src/components/screens/SendMailScreen.tsx
@@ -1,14 +1,16 @@
 import { Box, Button, Container, createTheme, CssBaseline, TextField, ThemeProvider, Typography } from "@mui/material";
+import axios from "axios";
 import { useCallback, useState } from "react";
+import process from 'node:process';
 
 const theme = createTheme();
 
 const SendMailScreen = () => {
-    const [form, setForm] = useState({
-        email: '',
-        asunto: '',
-        cuerpo: ''
-    });
+	const [form, setForm] = useState({
+	    email: '',
+	    asunto: '',
+	    cuerpo: ''
+	});
 
     const [validarMail, setValidarMail] = useState(false);
 
@@ -29,77 +31,94 @@ const SendMailScreen = () => {
 
     const handleSubmit = useCallback(async(e: any) => {
         e.preventDefault();
-        console.log(form);
+
+		if(!validarMail && asunto.length > 0 && cuerpo.length > 0){
+			try {
+				await axios({
+					url: `${import.meta.env.VITE_BACKEND_URL}/api/v1/mail/send`,
+					method: 'POST',
+					data: form
+				});
+
+				alert("Correo enviado correctamente");
+			} catch (error) {
+				alert("Error al enviar el correo");
+
+				console.log(error);
+			}
+		}else{
+			alert('Debe llenar todos los campos');
+		}
     }, [form]);
 
-  return (
-    <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-                <Typography component="h1" variant="h5">
-                    Enviar correo electronico
-                </Typography>
-                <Box 
-                    component="form"
-                    noValidate sx={{ mt: 1 }}
-                    onSubmit={handleSubmit}
-                >
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        label="Correo electronico"
-                        name="email"
-                        value={email}
-                        onChange={handleChange}
-                        autoComplete="email"
-                        autoFocus
-                        error={validarMail}
-                        helperText={validarMail ? 'Correo electronico no valido' : ''}
-                        type="email"
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        label="Asunto"
-                        name="asunto"
-                        value={asunto}
-                        onChange={handleChange}
-                        type="text"
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="cuerpo"
-                        label="Cuerpo del correo"
-                        type="text"
-                        value={cuerpo}
-                        onChange={handleChange}
-                        minRows={3}
-                        multiline 
-                    />
-                    <Button
-                      type="submit"
-                      fullWidth
-                      variant="contained"
-                      sx={{ mt: 3, mb: 2 }}
-                    >
-                      Enviar
-                    </Button>
-                </Box>
-            </Box>
-        </Container>
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider theme={theme}>
+		    <Container component="main" maxWidth="xs">
+		        <CssBaseline />
+		        <Box
+		          sx={{
+		            display: 'flex',
+		            flexDirection: 'column',
+		            alignItems: 'center',
+		          }}
+		        >
+		            <Typography component="h1" variant="h5">
+		                Enviar correo electronico
+		            </Typography>
+		            <Box 
+		                component="form"
+		                noValidate sx={{ mt: 1 }}
+		                onSubmit={handleSubmit}
+		            >
+		                <TextField
+		                    margin="normal"
+		                    required
+		                    fullWidth
+		                    label="Correo electronico"
+		                    name="email"
+		                    value={email}
+		                    onChange={handleChange}
+		                    autoComplete="email"
+		                    autoFocus
+		                    error={validarMail}
+		                    helperText={validarMail ? 'Correo electronico no valido' : ''}
+		                    type="email"
+		                />
+		                <TextField
+		                    margin="normal"
+		                    required
+		                    fullWidth
+		                    label="Asunto"
+		                    name="asunto"
+		                    value={asunto}
+		                    onChange={handleChange}
+		                    type="text"
+		                />
+		                <TextField
+		                    margin="normal"
+		                    required
+		                    fullWidth
+		                    name="cuerpo"
+		                    label="Cuerpo del correo"
+		                    type="text"
+		                    value={cuerpo}
+		                    onChange={handleChange}
+		                    minRows={3}
+		                    multiline 
+		                />
+		                <Button
+		                  type="submit"
+		                  fullWidth
+		                  variant="contained"
+		                  sx={{ mt: 3, mb: 2 }}
+		                >
+		                  Enviar
+		                </Button>
+		            </Box>
+		        </Box>
+		    </Container>
+		</ThemeProvider>
+	)
 }
 
 export default SendMailScreen;
